cae6d4d3ebe79566ebc27130817cec31ae842859
Weldon Carlos <weldonprogrammer2022@gmail.com>
2024-04-20 02:34:29 -0300

    codigo html formulario de login

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
   <div id="login-container">
     <h1>Login</h1>
     <form action="login.php"  method="POST">
-      <label for="email">E-mail</label>
-      <input type="email" name="email" id="email" placeholder="Digite seu e-mail" autocomplete="off">
-      <label for="password">Senha</label>
-      <input type="password" name="password" id="password" placeholder="Digite a sua senha">
+      <label for="email">Usuário</label>
+      <input type="text" name="usuario" id="email" placeholder="Digite seu e-mail" autocomplete="on">
+      <label for="senha">Senha</label>
+      <input type="password" name="senha" id="password" placeholder="Digite a sua senha">
       <a href="#" id="forgot-pass">Esqueceu a senha?</a>
       <input type="submit" value="Login">
     </form>
